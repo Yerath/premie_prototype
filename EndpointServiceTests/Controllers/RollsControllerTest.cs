1dc4b46ac53e588956431ff51c8b09fabb058d7f
Dimitry Volker <dimitry.volker@gmail.com>
2018-06-13 22:32:29 +0200

    Added more tests

---
+++ EndpointServiceTests/Controllers/RollsControllerTest.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+using EndpointService.Controllers;
+using EndpointService.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using Newtonsoft.Json;
+using RollsService.Interfaces;
+using Shouldly;
+
+namespace EndpointServiceTests.Controllers
+{
+    [TestClass]
+    public class RollsControllerTest
+    {
+        private RollsController _sut;
+        private Mock<IRollsService> _serviceMock;
+        private RollsService.Entities.PersonenautoWAMiniCasco _personenautoWaMiniCasco;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            _personenautoWaMiniCasco = CreatePersonenautoWaMiniCasco();
+
+            _serviceMock = new Mock<IRollsService>(MockBehavior.Strict);
+            _serviceMock.Setup(f => f.BerekenPremiePersonenAutoWAMiniCasco(It.IsAny<string>(), It.IsAny<DateTime>(),
+                    It.IsAny<string>()))
+                .Returns(
+                    Task.FromResult(_personenautoWaMiniCasco)
+                );
+
+            _sut = new RollsController(_serviceMock.Object);
+        }
+
+        [TestMethod]
+        public void EndpointShouldReturnThePersonenautoWAMiniCascoInJson()
+        {
+            var result = _sut.Index() as ContentResult;
+            var mappedObject = Mapper.Map<PersonenautoWAMiniCasco>(_personenautoWaMiniCasco);
+            result?.Content.ShouldBe(JsonConvert.SerializeObject(mappedObject));
+        }
+
+        private RollsService.Entities.PersonenautoWAMiniCasco CreatePersonenautoWaMiniCasco()
+        {
+            return new RollsService.Entities.PersonenautoWAMiniCasco
+            {
+                WAWettelijkeAansprakelijkheid =
+                    new RollsService.Entities.Dekking()
+                    {
+                        Beschrijving = "WA Wettelijke Aanspraakelijkheid dekking",
+                        Brutopremie = "300",
+                        DekkingCode = "AJSD1E",
+                        Naam = "WA"
+                    },
+                BMNoClaim =
+                    new RollsService.Entities.Dekking()
+                    {
+                        Beschrijving = "WA Wettelijke Aanspraakelijkheid dekking",
+                        Brutopremie = "300",
+                        DekkingCode = "AJSD1E",
+                        Naam = "WA"
+                    },
+                Casco =
+                    new RollsService.Entities.Dekking()
+                    {
+                        Beschrijving = "WA Wettelijke Aanspraakelijkheid dekking",
+                        Brutopremie = "300",
+                        DekkingCode = "AJSD1E",
+                        Naam = "WA"
+                    },
+                POI =
+                    new RollsService.Entities.Dekking()
+                    {
+                        Beschrijving = "WA Wettelijke Aanspraakelijkheid dekking",
+                        Brutopremie = "300",
+                        DekkingCode = "AJSD1E",
+                        Naam = "WA"
+                    },
+                Rechtsbijstands =
+                    new RollsService.Entities.Dekking()
+                    {
+                        Beschrijving = "WA Wettelijke Aanspraakelijkheid dekking",
+                        Brutopremie = "300",
+                        DekkingCode = "AJSD1E",
+                        Naam = "WA"
+                    },
+            };
+        }
+
+
+    }
+}
---
+++ EndpointServiceTests/Controllers/VPIControllerTest.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using EndpointService.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using Shouldly;
+using VPIService.Interfaces;
+
+namespace EndpointServiceTests.Controllers
+{
+    [TestClass]
+    public class VPIControllerTest
+    {
+        private VPIController _sut;
+        private Mock<IVPIService> _serviceMock;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            _serviceMock = new Mock<IVPIService>(MockBehavior.Strict);
+            _serviceMock.Setup(f => f.BerekenPremie()).Returns(Task.FromResult("message"));
+
+            _sut = new VPIController(_serviceMock.Object);
+        }
+
+        [TestMethod]
+        public void ControllerShouldReturnResultFromService()
+        {
+            var result = _sut.Index() as ContentResult;
+            result?.Content.ShouldBe("message");
+        }
+
+    }
+}
